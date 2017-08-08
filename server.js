a34fa483a6f23f5ff5a47997609884037e981f0e
JSLinn727 <justinslinn727@gmail.com>
2017-08-07 20:47:29 -0400

    half way, need to wire template

--- server.js
+++ server.js
@@ -3,27 +3,43 @@ const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const expressSession = require('express-session')
 const expressValidator = require('express-validator')
+const fs = require('fs')
+const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
 
 const app = express()
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(expressValidator())
 app.use(express.static('public'))
 app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}))
-
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+let wordGen = function() {
+  let i = Math.floor(Math.random() * words.length)
+  return words[i]
+}
+
+const mystery = word => {
+  return word.split('').map(letter => {
+    return (letter = '  ___  ')
+  }).join('')
+}
+
 app.get('/', (req, res) => {
+  game = req.session
+  let word = wordGen()
+  game.gameWord = word
+  game.mysteryWord = mystery(word)
+  console.log(req.session.gameWord)
+  res.render('home', game)
+})
 
-  const placeholder = '_'
-
-  const hangman = (word, guess) => {
-      
-  }
-  res.render('home')
+app.post('/guess', (req, res) => {
+  game = req.session
+  game.guessedLetter = req.body.letter
+  res.render('home', game)
 })
 
 app.listen(3000, () => {
